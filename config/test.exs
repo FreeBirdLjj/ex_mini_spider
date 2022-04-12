fb07ffefdccf9b64f371cfac54498f52c38e9e73
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2022-04-13 00:30:50 +0800

    Enable GitHub Actions.

--- config/test.exs
+++ config/test.exs
@@ -5,8 +5,8 @@ config :mini_spider, MiniSpider.Repo,
   database: "postgres",
   username: "postgres",
   password: "postgres",
-  hostname: "127.0.0.1",
-  port: "5432"
+  hostname: System.get_env("POSTGRES_HOST") || "127.0.0.1",
+  port: System.get_env("POSTGRES_PORT") || "5432"
 
 config :mini_spider, Oban,
   repo: MiniSpider.Repo,
--- test/mini_spider_test.exs
+++ test/mini_spider_test.exs
@@ -1,8 +1,4 @@
 defmodule MiniSpiderTest do
   use ExUnit.Case
   doctest MiniSpider
-
-  test "greets the world" do
-    assert MiniSpider.hello() == :world
-  end
 end
